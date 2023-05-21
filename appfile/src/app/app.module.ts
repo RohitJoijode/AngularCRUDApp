5d788b5314159eaeb5727f2a77f0e89abcce8608
@RohitJoijode <rohitjoijode122@gmail.com>
2023-05-21 21:30:32 +0530

    Commited by rohit joijode

---
+++ appfile/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { AdventureDetailsComponent } from './adventure-details/adventure-details.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CustomErrorComponent } from './custom-error/custom-error.component';
+//import { EmployeeFormComponent } from './employee-details/employee-form/employee-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeDetailsComponent,
+    AdventureDetailsComponent,
+    LogInComponent,
+    CustomErrorComponent
+    //EmployeeFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
